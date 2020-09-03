2534e31853dda93f58c953a74ae77120494b068c
higor <higor.santos@aluno.faculdadeimpacta.com.br>
2020-09-03 18:18:18 -0300

    AC1

---
+++ src/jogovelha.py
@@ -0,0 +1,12 @@
+def inicializar():
+        tab = ( )
+        for = in range(3):
+            linha = ( )
+            for j in range(3):
+                    linha.append(".")
+            tab.append(linha)
+        return tab
+
+def main( ):
+    	jogo = inicializar( )
+    	print (jogo)
